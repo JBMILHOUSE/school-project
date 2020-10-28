e498941653dd08dcb2a2076921ced66801697724
JBMILHOUSE <43553799+JBMILHOUSE@users.noreply.github.com>
2020-10-28 18:41:52 -0300

    caromêtro

--- src/components/Carometro/index.js
+++ src/components/Carometro/index.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import './style.css';
+
+const apiUrl = 'http://localhost:5000/api/aluno';
+var dadosAlunos = [];
+
+const imgUrl = 'https://github.com/JBMILHOUSE/images/blob/main/';
 
 export default class Carometro extends Component {
+  
+  componentDidMount() {
+    fetch(apiUrl)
+       .then(res => res.json())
+       .then((result) => {
+         dadosAlunos = result;
+         console.log("dadosAlunos:" + result);
+       })
+  }
+  
   render() {
     return(
+      <div>
       <h1>Carômetro</h1>
+      {
+        dadosAlunos.map(
+          (aluno) => 
+            <div className="card" key={aluno.id}>
+             <img src={`${imgUrl}/${aluno.ra}.png?raw=true`}  alt={aluno.ra} />    
+
+             <div className="container">
+               <h4><b>{aluno.ra}</b></h4>
+               <p>{aluno.nome}</p>
+             </div>
+            </div>
+       )
+      }
+     </div>
     )
   }
 }
+
